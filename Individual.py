b89e5615c6420ddc175904b7490790c5ba856ad0
Bartosz Rosiak <barti197@interia.pl>
2019-12-08 00:43:58 +0100

    added fitness function count and some elements of genetic algorithm

--- Individual.py
+++ Individual.py
@@ -1,64 +1,121 @@
 import Flight
+import random
+import copy
+import numpy as np
 class Individual:
     def __init__(self):
-        self.__flights = []
-        self.__departure_flights = []
-        self.__approach_flights = []
-        self.__fitness = 0
-        self.__runway_throughput = 0
-        self.__flight_losses = 0
-        self.__robustness = 0
+        self.flights = []
+        self.departure_flights = []
+        self.approach_flights = []
+        self.fitness = 0
+        self.runway_throughput = 0
+        self.flight_losses = 0
+        self.robustness = 0
+        self.runway_throughput_ranking = 0
+        self.flight_losses_ranking = 0
+        self.robustness_ranking = 0
+        self.individual_ranking = {}
 
     def init_all_flights(self, approach_data, departure_data):
-        self.__departure_flights = init_flights(departure_data, 'departure')
-        self.__approach_flights = init_flights(approach_data, 'approach')
-        self.__flights = self.__approach_flights + self.__departure_flights
+        self.departure_flights = init_flights(departure_data, 'departure')
+        self.approach_flights = init_flights(approach_data, 'approach')
+        self.flights = self.approach_flights + self.departure_flights
+    
+    # def set_flights(self, flights):
+    #     self.__flights = flights
+    
+    # def get_flights(self):
+    #     return self.__flights
+
+    def get_queue(self):
+        flights = self.flights.copy()
+        for i in range(len(flights)):
+            num = flights[i].sequence_number
+            flights[i] = num
+        return flights
+    
+    
+            
+    
+
     
 
 
     def calc_runway_throughput(self):
-        pass
+        """calculate runway throughput of flight queue
+        
+        Returns:
+            int -- runway throughput
+        """
+        self.runway_throughput = self.flights[-1].get_actual_time_s() - self.flights[0].get_actual_time_s()
+        return self.runway_throughput
 
     def calc_flight_losses(self, delta_t):
+        """calculate flight delay losses of flight queue
+        
+        Arguments:
+            delta_t {int} -- permissible delay of flight
+        
+        Returns:
+            double -- flight delay losses
+        """
         losses = 0
-        for i in range(len(self.__flights)):
-            if(self.__flights[i].is_delayed(delta_t)):
-                losses += (self.__flights[i].get_delay_time() -delta_t) * \
-                    self.__flights[i].get_unit_loss()
+        for i in range(len(self.flights)):
+            if(self.flights[i].is_delayed(delta_t)):
+                losses += (self.flights[i].delay_time -delta_t) * \
+                    self.flights[i].unit_loss
             else:
                 continue
-        self.__flight_losses = losses
+        self.flight_losses = losses
         return losses
 
 
     def calc_robustness(self, delta_t):
+        """calculate robustness of flight queue
+        
+        Arguments:
+            delta_t {int} -- permissible delay of flight
+        
+        Returns:
+            double -- robustness
+        """
         robustness = 0
-        for i in range(len(self.__flights)):
-            if(self.__flights[i].is_delayed(delta_t)):
+        for i in range(len(self.flights)):
+            if(self.flights[i].is_delayed(delta_t)):
                 robustness += 1
             else:
                 continue
-        self.__robustness = robustness
+        self.robustness = robustness
         return robustness
 
-    def calculate_fitness(self):
+    def calculate_objective_end(self, k, N, parameter):
+        if parameter==1:
+            return k * N ** 2
+        else:
+            return (N - parameter) ** 2
+
+    def calculate_fitness(self, k, N):
+        fitness = 0
+        for i in self.individual_ranking:
+            fitness += self.calculate_objective_end(k, N, self.individual_ranking.get(i))
+        self.fitness = fitness
+        #if(check_parameter(self.runway_throughput_ranking))
         #self.fitness = self.calc_runway_throughput() + self.calc_flight_losses() + self.calc_robustness()
-        pass
 
-    def get_fintess(self):
-        return self.__fitness
+    # def get_fintess(self):
+    #     return self.__fitness
 
-    def get_runway_throughput(self):
-        return self.__runway_throughput
+    # def get_runway_throughput(self):
+    #     return self.__runway_throughput
 
-    def get_flight_losses(self):
-        return self.__flight_losses
+    # def get_flight_losses(self):
+    #     return self.__flight_losses
 
-    def get_robustness(self):
-        return self.__robustness
+    # def get_robustness(self):
+    #     return self.__robustness
 
     def get_robustness_percentage(self):
-        return ((self.get_robustness() / len(self.__flights)) *100)
+        return ((self.robustness / len(self.flights)) *100)
 
 def init_flights(data, flight_type):
     flights = []
@@ -83,3 +140,36 @@ def init_flights(data, flight_type):
         flights.append(flight)
     return flights
 
+# def prepare(n):
+#     population = [0 for i in range(n)]
+#     return population
+
+# def marriage(population, ro):
+#     parents = prepare(ro)
+#     selected_individuals = np.random.permutation(len(population))
+#     for i in range(ro):
+#         parents[i] = population[selected_individuals[i]]
+#     return parents
+
+# # =============================================================================
+# # def initialize(population_size, individual):
+# #     population = prepare(population_size)
+# #     flights = individual.get_flights()
+# #     for i in (random.sample(flights,len(flights))):
+# #         population[i] = copy.deepcopy(individual)
+# #         #flights = individual.get_flights()
+# #         population[i].set_flights(flights[i])
+# #     return population
+# # =============================================================================
+
+# def initialize_population(population_size, approach_data, departure_data):
+#     population = []
+#     for i in range(population_size):
+#         individual = Individual()
+#         individual.init_all_flights(approach_data, departure_data)
+#         a = individual.flights
+#         a = random.sample(a,len(a))
+#         individual.flights = a
+#         #individual.get_flights()
+#         population.append(individual)
+#     return population
--- program.py
+++ program.py
@@ -1,25 +1,59 @@
 import Individual
+import Genetic_algorithm
 import pandas as pd
+import copy
 from os import strerror
+import time
 
 '''variables'''
 approach_name = "approach_initial.txt"
 departure_name = "departure_initial.txt"
 delta_t = 120
+runways = 2
+crossover_probability = 0.9
+mutation_probability = 0.1
+generation_gap = 0.9
+elimination_rate = 0.2
+alfa = 0.2
+population_size = 100
+error = 2500
+k = 1.5
+vk_min = 1
+vk_max = 30
+objective_functions = ['runway_throughput', 'flights_losses', 'robustness']
 '''variables'''
 
 
 
 
 if __name__ == "__main__":
-    try:    
-        approach_data = pd.read_table(approach_name)
-        departure_data = pd.read_table(departure_name)
-    except IOError as exc:
-        print("Error occured ", strerror(exc.errno))
-        exit(exc.errno)
-    individual = Individual.Individual()
-    individual.init_all_flights(approach_data, departure_data)
-    print(individual.calc_flight_losses(delta_t))
-    print(individual.calc_robustness(delta_t))    
-    print("robustness is ",round(individual.get_robustness_percentage(), 2),"%")
+    # try:    
+    #     approach_data = pd.read_table(approach_name)
+    #     departure_data = pd.read_table(departure_name)
+    # except IOError as exc:
+    #     print("Error occured ", strerror(exc.errno))
+    #     exit(exc.errno)
+    # individual = Individual.Individual()
+    # individual.init_all_flights(approach_data, departure_data)
+    # print(individual.calc_flight_losses(delta_t))
+    # print(individual.calc_robustness(delta_t))    
+    # print("robustness is ",round(individual.get_robustness_percentage(), 2),"%")
+    # print(individual.calc_runway_throughput())
+    # print(individual.get_queue())
+    # start = time.time()
+    # population = Individual.initialize_population(population_size, approach_data, departure_data)
+    # end = time.time()
+    # print("without copying: ",end-start)
+    # print(population[0].get_queue())
+    genetic_algorithm = Genetic_algorithm.Genetic_algorithm(crossover_probability = crossover_probability, mutation_probability = mutation_probability, \
+        generation_gap = generation_gap, elimination_rate = elimination_rate, alfa = alfa, population_size = population_size, \
+            error = error, k = k, vk_min = vk_min, vk_max = vk_max, delta_t = delta_t, runways = runways)
+    genetic_algorithm.assign_initial_data(approach_name, departure_name)
+    genetic_algorithm.initialize_population(approach_name, departure_name)
+    genetic_algorithm.call_all_objectives_population()
+    genetic_algorithm.assign_rankings(objective_functions, genetic_algorithm.population)
+    genetic_algorithm.population[0].calculate_fitness(k = k, N = population_size)
+    print(genetic_algorithm.population[0].fitness)
+    
+            
+  
